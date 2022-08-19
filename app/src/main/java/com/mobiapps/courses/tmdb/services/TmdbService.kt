bafa6db6c4ae3d0b11ae48c33cb07ae8c9455dc2
Simon Bernardin <simon.bernardin@mobiapps.fr>
2022-08-19 16:09:27 +0200

    fix: ajout de callback dans le service

--- app/src/main/java/com/mobiapps/courses/tmdb/services/TmdbService.kt
+++ app/src/main/java/com/mobiapps/courses/tmdb/services/TmdbService.kt
@@ -1,17 +1,20 @@
 package com.mobiapps.courses.tmdb.services
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import com.mobiapps.courses.tmdb.datasources.MockDataSource
-import com.mobiapps.courses.tmdb.entities.CastMember
 import com.mobiapps.courses.tmdb.entities.Movie
 
 class TmdbService {
     private val dataSource = MockDataSource()
 
-    fun getMovieDetail(id: Int): Movie? {
-        return dataSource.movies.find { it.id == id }
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun getMovieDetail(id: Int, success: (movie: Movie?) -> Unit, failure: () -> Unit) {
+        success(dataSource.movies.find { it.id == id })
     }
 
-    fun getLatestMovies(): List<Movie> {
-        return dataSource.movies
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun getLatestMovies(success: (movies: List<Movie>) -> Unit, failure: () -> Unit) {
+        success(dataSource.movies)
     }
 }
